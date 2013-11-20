6a0d4f7cc6072f821137110953e85e941211302e
matrixatp.martin@gmail.com <>
2013-11-20 19:29:32 +0000

    codenvy sync

--- view/controlPanel.php
+++ view/controlPanel.php
@@ -16,6 +16,7 @@
             <br>
             <br>
             <br>
+            <br>
             
             <input type="submit" name="submit" value="Submit"/>
         </form>
--- view/register.php
+++ view/register.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     <div class="newUserForm">
         
         <form action="?action=register_user" method="post">
@@ -12,19 +11,4 @@
        
             <input type="submit" name="register" value="Register"/>
         </form>
-=======
-    <div class="newUserForm">
-        
-        <form action="?action=register_user" method="post">
-            <label name="new_user_name">Username: </label><br>
-            <input type="text" name="new_user_name" />
-            <br><br>
-
-            <label name="new_user_pass">Password: </label><br>
-            <input type="password" name="new_user_pass" />
-            <br><br>
-       
-            <input type="submit" name="register" value="Register"/>
-        </form>
->>>>>>> refs/remotes/origin/master
     </div>
