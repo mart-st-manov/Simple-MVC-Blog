908a7c7c2d0e83dd691de583124ef91836d65b42
Martin Manov <mart.st.manov@gmail.com>
2013-12-03 19:05:13 +0200

    remove unnecessary model

--- controller/controlPanelController.php
+++ controller/controlPanelController.php
@@ -1,7 +1,6 @@
 <?php
     class ControlPanelController {
         public function __construct() {
-            $this->model = new ControlPanelModel();
             include 'view/controlPanel.php';
         }
     }
