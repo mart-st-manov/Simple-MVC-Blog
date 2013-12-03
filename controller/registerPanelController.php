a52689b490845bc1852cc3b28488f21a11463972
Martin Manov <mart.st.manov@gmail.com>
2013-12-03 18:56:51 +0200

    Delete registerPanelController.php

--- controller/registerPanelController.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class RegisterPanelController {
-        public function __construct() {
-            $this->model = new RegisterPanelModel();
-            include 'view/register.php';
-        }
-    }
-?>
